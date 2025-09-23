78ce0590a51af038702bfbfcba93d79d72ad02b0
MSKatKing <mark@dinoslice.com>
2025-09-22 23:18:01 -0500

    Improve comments on SerialDMXTransmitter::send_break

--- src/serial/tx.rs
+++ src/serial/tx.rs
@@ -1,6 +1,9 @@
 /// This trait should be implemented on any type that can interface with a raw physical DMX port.
 pub trait SerialDMXTransmitter {
-    /// This method should send a LOW value for at least 88us followed by a HIGH value for at least 8us
+    /// Sends the DMX 'break' and 'mark-after-break' (MAB) signals.
+    ///
+    /// This method should hold the line LOW for at least 88 microseconds (DMX break),
+    /// followed by HIGH for at least 8 microseconds (mark-after-break).
     fn send_break(&mut self);
 
     /// This method should write the bytes of data to the DMX port one at a time.
