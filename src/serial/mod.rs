9c707dc74c4d5cb8c05eb368dce3c5753986eb95
MSKatKing <mark@dinoslice.com>
2025-09-22 23:18:16 -0500

    Improve implementation of send_serial_dmx

--- src/serial/mod.rs
+++ src/serial/mod.rs
@@ -3,6 +3,11 @@ mod tx;
 pub use tx::*;
 
 pub fn send_serial_dmx(transmitter: &mut dyn SerialDMXTransmitter, data: &[u8]) {
+    let mut dmx_frame = [0u8; 513];
+    let data_len = data.len().min(512);
+
+    dmx_frame[1..=data_len].copy_from_slice(&data[..data_len]);
+
     transmitter.send_break();
-    transmitter.write(data);
+    transmitter.write(&dmx_frame);
 }
