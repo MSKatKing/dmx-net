9b23e9fe1630a0fa0b0d1a6a2fe262098f9ac85d
MSKatKing <mark@dinoslice.com>
2025-09-22 23:06:01 -0500

    Add features for art_net and sacn

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,21 @@
 
 #[cfg(feature = "serial")]
 mod serial;
+
+#[cfg(feature = "art_net")]
 mod art_net;
+
+#[cfg(feature = "sacn")]
 mod sacn;
 
 #[cfg(feature = "serial")]
 pub use serial::*;
+
+#[cfg(feature = "art_net")]
+pub use art_net::*;
+
+#[cfg(feature = "sacn")]
+pub use sacn::*;
 
 #[cfg(test)]
 mod tests {
